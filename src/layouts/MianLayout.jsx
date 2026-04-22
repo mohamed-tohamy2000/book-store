62a9bdcdab162a0d2b9ec1572cc218b4a1a992a5
mando-1993 <workm1479@gmail.com>
2026-04-22 22:34:48 +0200

    Fix footer import case for Vercel build

--- src/layouts/MianLayout.jsx
+++ src/layouts/MianLayout.jsx
@@ -1,6 +1,6 @@
 import { Outlet } from "react-router-dom";
 import Navbar from "../components/Navbar/Navbar";
-import Footer from "../components/footer/footer";
+import Footer from "../components/Footer/Footer";
 
 
 export default function Mainlayout() {
